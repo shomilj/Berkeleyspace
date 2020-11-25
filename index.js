fa38b370d0648ded8effc7463283f435f0cfbc96
Shomil Jain <shomil@berkeley.edu>
2020-11-25 14:45:09 -0800

    updated build files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
 /* Fuse is our fuzzy searching API. */
 const Fuse = require('fuse.js')
 
